df85a91bb3277406168826b510e1462d12dde61c
tarasomel <iamnato4ka@gmail.com>
2016-04-22 12:00:03 +0300

    Translated

---
+++ resources/lang/uk/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Пароль повинен містити не менше шести символів і відповідати тому підтвердження.',
+    'reset' => 'Ваш пароль скинутий!',
+    'sent' => 'У нас є по електронній пошті посилання на відновлення пароля!',
+    'token' => 'Цей пароль скидання маркера є недійсним.',
+    'user' => "Ми не можемо знайти користувача з адресою електронної пошти.",
+
+];

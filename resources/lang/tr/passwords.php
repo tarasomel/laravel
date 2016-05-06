9a31b8b9510f94ea03195367bd39146825b39ae0
tarasomel <iamnato4ka@gmail.com>
2016-05-06 15:10:32 +0300

    Translated

---
+++ resources/lang/tr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Şifreler en az 6 karakter olmalı ve onay eşleşmesi gerekir.',
+    'reset' => 'Şifreniz sıfırlandı!',
+    'sent' => 'E-şifrenizi sıfırlama linki gönderilecek var!',
+    'token' => 'Bu parola, token geçersiz sıfırlayın.',
+    'user' => "Bu e-posta adresi ile bir kullanıcı bulabiliriz.",
+
+];

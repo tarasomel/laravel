6080e322add9d22c971359f827138e10a8e90e93
tarasomel <iamnato4ka@gmail.com>
2016-05-06 15:11:09 +0300

    Translated

---
+++ resources/lang/vi/auth2.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Những kỹ năng nào không khớp với hồ sơ của chúng.',
+    'throttle' => 'Quá nhiều đăng nhập nỗ lực. Xin vui lòng thử lại. giây giây.',
+
+];

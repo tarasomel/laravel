b55ff2d499a77a0a58a421fd9fd4f68bf94a277a
tarasomel <iamnato4ka@gmail.com>
2016-05-06 15:10:15 +0300

    Translated

---
+++ resources/lang/zh/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => '密码必须至少六角色和匹配的确认。',
+    'reset' => '你的密码就一直是重!',
+    'sent' => '我发电邮给你的密码重置系!',
+    'token' => '这一象征性的密码重置也是无效的。',
+    'user' => "我们找不到一个用户的邮件地址。",
+
+];

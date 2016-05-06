39ea130253bb8154c77b95b8980ab35b6b974a7b
tarasomel <iamnato4ka@gmail.com>
2016-05-06 15:10:48 +0300

    Translated

---
+++ resources/lang/ja/validation.php
@@ -0,0 +1,113 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'accepted'             => 'の属性を必ずお受けできます。',
+    'active_url'           => 'または属性が変わってきているんですけど。',
+    'after'                => 'の属性でなければな日後の日です。',
+    'alpha'                => 'の属性だけが含まれます。',
+    'alpha_dash'           => 'の属性に含まれるのは、英字、数字、ダッシュします。',
+    'alpha_num'            => 'の属性に含まれるのを見学しました。',
+    'array'                => 'の属性でなければな配列になります。',
+    'before'               => 'の属性でなければな日前の日です。',
+    'between'              => [
+        'numeric' => 'の属性を指定する画面が表示されmin、最大です。',
+        'file'    => 'の属性を指定する画面が表示されmin、最大キロバイトです。',
+        'string'  => 'の属性を指定する画面が表示されmin、最大文字ます。',
+        'array'   => 'の属性が必要とminとmaxます。',
+    ],
+    'boolean'              => 'の属性がtrueでなければなりませんまたはfalseです。',
+    'confirmed'            => 'の属性確認が一致しませんでした。',
+    'date'                 => 'または属性が変わってきているんですけど。',
+    'date_format'          => 'の属性に合っていないフォーマット：形式です。',
+    'different'            => 'の属性およびその他の必要なければならないと思います。',
+    'digits'               => 'の属性を必:桁桁を表します。',
+    'digits_between'       => 'の属性を指定する画面が表示されmin、最大桁を表します。',
+    'distinct'             => 'の属性分野では、複製値です。',
+    'email'                => 'の属性でなければなメールアドレスが正しく入力します。',
+    'exists'               => 'の選択した属性は無効となります。',
+    'filled'               => 'の属性が必要です。',
+    'image'                => 'の属性でなければなイメージです。',
+    'in'                   => 'の選択した属性は無効となります。',
+    'in_array'             => 'の属性が存在しないとする。',
+    'integer'              => 'の属性は整数直定数である必要があります。',
+    'ip'                   => 'の属性は、有効なIPアドレスです。',
+    'json'                 => 'の属性は、有効なJSON文字列になります。',
+    'max'                  => [
+        'numeric' => 'の属性は以上となります。',
+        'file'    => 'の属性は以上の最大数です。',
+        'string'  => 'の属性は以上の最大文字ます。',
+        'array'   => 'の属性がない以上：最大品目です。',
+    ],
+    'mimes'                => 'の属性でなければなファイルのタイプ::数値です。',
+    'min'                  => [
+        'numeric' => 'の属性が必要なものにな分です。',
+        'file'    => 'の属性が必要なものになる分数です。',
+        'string'  => 'の属性が必要なものにな分文字です。',
+        'array'   => 'の属性が必要なものにな分項目です。',
+    ],
+    'not_in'               => 'の選択した属性は無効となります。',
+    'numeric'              => 'の属性でなければな番号です。',
+    'present'              => 'の属性が存在している必要があります。',
+    'regex'                => 'の属性の形式を取り出すことができます。',
+    'required'             => 'の属性が必要です。',
+    'required_if'          => 'の属性が必要な場合:その他:値です。',
+    'required_unless'      => 'の属性が必要なその他は価値です。',
+    'required_with'        => 'の属性が必要な場合の値です。',
+    'required_with_all'    => 'の属性が必要な場合の値です。',
+    'required_without'     => 'の属性が必要な価値がありません。',
+    'required_without_all' => 'の属性を必要しない場合の値です。',
+    'same'                 => 'の属性その他一致している必要があります。',
+    'size'                 => [
+        'numeric' => 'の属性を必ずサイズです。',
+        'file'    => 'の属性が必要サイズをキロバイトです。',
+        'string'  => 'の属性を必ず小文字です。',
+        'array'   => 'の属性が含まれている必要があ:サイズです。',
+    ],
+    'string'               => 'の属性は文字列でなければならない。',
+    'timezone'             => 'の属性は、有効なゾーンです。',
+    'unique'               => 'の属性です。',
+    'url'                  => 'の属性の形式を取り出すことができます。',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify custom validation messages for attributes using the
+    | convention "attribute.rule" to name the lines. This makes it quick to
+    | specify a specific custom language line for a given attribute rule.
+    |
+    */
+
+    'custom' => [
+        'attribute-name' => [
+            'rule-name' => 'カスタムメッセージ',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap attribute place-holders
+    | with something more reader friendly such as E-Mail Address instead
+    | of "email". This simply helps us make messages a little cleaner.
+    |
+    */
+
+    'attributes' => [],
+
+];

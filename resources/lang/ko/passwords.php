fa17df9ffa066640d33507da780c8bd777ac796d
tarasomel <iamnato4ka@gmail.com>
2016-05-06 15:10:10 +0300

    Translated

---
+++ resources/lang/ko/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => '암호의 수와 일치 확인합니다.',
+    'reset' => '암호를 다시 설정되었다!',
+    'sent' => '우리는 전자 우편으로 암호를 다시 설정됩니다.',
+    'token' => '이 암호를 다시 설정 토큰은 잘못되었습니다.',
+    'user' => "우리는 우리를 찾을 수 없는 사용자는 전자 메일 주소입니다.",
+
+];

e8eedb8d8048c2c9178ff18fdb9e59cc7b32d92c
tarasomel <iamnato4ka@gmail.com>
2016-05-06 15:10:04 +0300

    Translated

---
+++ resources/lang/he/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'סיסמאות חייב להיות לפחות שישה תווים ולהתאים את האישור.',
+    'reset' => 'הסיסמה שלך אופסו!',
+    'sent' => 'יש לנו אי-מייל שלך לאיפוס הסיסמה הקישור!',
+    'token' => 'זה איפוס הסיסמה אסימון אינו חוקי.',
+    'user' => "אנחנו לא יכולים למצוא משתמש עם כתובת דואר אלקטרוני.",
+
+];

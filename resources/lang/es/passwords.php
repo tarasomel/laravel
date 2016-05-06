e225554ea1917d7ff7ba770aff5940c7eed7138e
tarasomel <iamnato4ka@gmail.com>
2016-05-06 15:10:30 +0300

    Translated

---
+++ resources/lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Las contraseñas deben tener al menos seis caracteres y el partido de la confirmación.',
+    'reset' => 'Su contraseña se ha restablecido!',
+    'sent' => 'Tenemos por correo electrónico el enlace de restablecimiento de contraseña!',
+    'token' => 'Este token de recuperación de contraseña no es válida.',
+    'user' => "No podemos encontrar un usuario con ese e-mail.",
+
+];

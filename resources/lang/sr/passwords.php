cb5084890196c2b310807a416e76a8b72ecd3892
tarasomel <iamnato4ka@gmail.com>
2016-05-06 15:10:30 +0300

    Translated

---
+++ resources/lang/sr/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Лозинка мора да садржи најмање шест знакова и одговара доказ.',
+    'reset' => 'Ваша лозинка је ресетована!',
+    'sent' => 'Имамо е-маил линк на опоравак лозинке!',
+    'token' => 'Ову лозинку ресет токен је неважећи.',
+    'user' => "Ми не можемо да нађемо корисника са том адресом е-поште.",
+
+];

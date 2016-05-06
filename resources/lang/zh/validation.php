e0013e04f89970e60e5691a9624bf7b4cb0bc9ed
tarasomel <iamnato4ka@gmail.com>
2016-05-06 15:10:28 +0300

    Translated

---
+++ resources/lang/zh/validation.php
@@ -0,0 +1,113 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines contain the default error messages used by
+    | the validator class. Some of these rules have multiple versions such
+    | as the size rules. Feel free to tweak each of these messages here.
+    |
+    */
+
+    'accepted'             => '的归属，就必须接受。',
+    'active_url'           => '第一：特征是不是有效的统一资源定位器连接。',
+    'after'                => '重点将必须是一个日期以后：日期。',
+    'alpha'                => '的归属，可以只包含的信',
+    'alpha_dash'           => '的归属，可能只有含有字母数字和划的。',
+    'alpha_num'            => '的归属，可能只有含有字母和数字',
+    'array'                => '的归属，就必须有一系列的。',
+    'before'               => '重点将必须是一个日前的日期。',
+    'between'              => [
+        'numeric' => '的归属，就必须：min：麦克斯。',
+        'file'    => '的归属，就必须：min和马克斯字节的。',
+        'string'  => '的归属，就必须：min和马克斯的角色',
+        'array'   => '的归属，就必须有一个：min和马克斯的项目。',
+    ],
+    'boolean'              => '于：将实地必须是真正的或虚假的。',
+    'confirmed'            => '的归属，确认并不匹配。',
+    'date'                 => '第一：特征是不是有效的日期。',
+    'date_format'          => '第一：特征不符的形式：格式。',
+    'different'            => '第一：特征和其他的必须是不同的。',
+    'digits'               => '重点将必须是：位位数。',
+    'digits_between'       => '的归属，就必须：min和max位数。',
+    'distinct'             => '有：赋予领域有重复的价值。',
+    'email'                => '重点将必须是一个有效的电邮地址。',
+    'exists'               => '在选定：将是无效的。',
+    'filled'               => '于：将实地需要。',
+    'image'                => '重点将必须是一个形象。',
+    'in'                   => '在选定：将是无效的。',
+    'in_array'             => '的归属领域并不存在：其他人。',
+    'integer'              => '重点将必须是一个整数而开心了',
+    'ip'                   => '重点将必须是一个有效的IP地址',
+    'json'                 => '重点将必须是一个有效的JSON绳子',
+    'max'                  => [
+        'numeric' => '的归属，可能不超过：麦克斯。',
+        'file'    => '的归属，可能不超过:max字节的。',
+        'string'  => '的归属，可能不超过:马克斯的角色',
+        'array'   => '的归属，可能并没有超过:max项目。',
+    ],
+    'mimes'                => '重点将必须是一个的档案的类型：是：价值观。',
+    'min'                  => [
+        'numeric' => '的归属，就必须至少：min.',
+        'file'    => '的归属，就必须至少：min字节的。',
+        'string'  => '的归属，就必须至少：min角色',
+        'array'   => '的归属，就必须有至少：min项目。',
+    ],
+    'not_in'               => '在选定：将是无效的。',
+    'numeric'              => '重点将必须是一个数字。',
+    'present'              => '于：将实地人员必须在场。',
+    'regex'                => '第一：特征的形式是无效的。',
+    'required'             => '于：将实地需要。',
+    'required_if'          => '于：将实地需要等其他：价值。',
+    'required_unless'      => '第一：特征的领域是必须的，除非：其他是：价值观。',
+    'required_with'        => '于：将实地需要的时候：价值是存在的。',
+    'required_with_all'    => '于：将实地需要的时候：价值是存在的。',
+    'required_without'     => '于：将实地需要的时候：价值是不存在的。',
+    'required_without_all' => '于：将实地需要的时候都没有：价值。',
+    'same'                 => '有：归因和其他必须符合。',
+    'size'                 => [
+        'numeric' => '重点将必须是：规模。',
+        'file'    => '重点将必须是：数字节的。',
+        'string'  => '重点将必须是：大小角色',
+        'array'   => '第一：特征必须包括：规模项目。',
+    ],
+    'string'               => '的归属，就必须有一条线。',
+    'timezone'             => '重点将必须是一个有效的区域。',
+    'unique'               => '的归属，已经采取。',
+    'url'                  => '第一：特征的形式是无效的。',
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | Here you may specify custom validation messages for attributes using the
+    | convention "attribute.rule" to name the lines. This makes it quick to
+    | specify a specific custom language line for a given attribute rule.
+    |
+    */
+
+    'custom' => [
+        'attribute-name' => [
+            'rule-name' => '定制的信息',
+        ],
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Custom Validation Attributes
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used to swap attribute place-holders
+    | with something more reader friendly such as E-Mail Address instead
+    | of "email". This simply helps us make messages a little cleaner.
+    |
+    */
+
+    'attributes' => [],
+
+];

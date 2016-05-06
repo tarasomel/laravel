d6bd7d9e291990c1f310ebae2382851a814da070
tarasomel <iamnato4ka@gmail.com>
2016-05-06 15:10:10 +0300

    Translated

---
+++ resources/lang/no/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Passord må være minst seks tegn og matche bekreftelse.',
+    'reset' => 'Passordet er tilbakestilt!',
+    'sent' => 'Vi har e-post for å tilbakestille passordet link!',
+    'token' => 'Dette for å tilbakestille passord token er ugyldig.',
+    'user' => "Vi kan ikke finne en bruker med denne e-postadressen.",
+
+];

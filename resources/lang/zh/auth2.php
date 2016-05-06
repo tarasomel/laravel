d624b34890b8ae8bb7c5d9d0a90987ff67254a49
tarasomel <iamnato4ka@gmail.com>
2016-05-06 15:10:52 +0300

    Translated

---
+++ resources/lang/zh/auth2.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => '这封信构成适当的临时全权证书并不相匹配我们的记录。',
+    'throttle' => '太多的登陆的企图。 请再试一次：秒秒',
+
+];

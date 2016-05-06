ecc904e8893fd2a95200907da9f8c07a43b2f001
tarasomel <iamnato4ka@gmail.com>
2016-05-06 15:10:14 +0300

    Translated

---
+++ resources/lang/ca/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Les contrasenyes han de tenir com a mínim sis caràcters i coincideix amb la confirmació.',
+    'reset' => 'La seva contrasenya ha estat reset!',
+    'sent' => 'Hem enviat per correu electrònic el vostre enllaç per restablir la contrasenya!',
+    'token' => 'Aquest restabliment de la contrasenya token no és vàlid.',
+    'user' => "No podem trobar un usuari amb aquest e-mail.",
+
+];

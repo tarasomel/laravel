a0eb5bed3bb2741e5383218ed82db6199ce1ae17
tarasomel <iamnato4ka@gmail.com>
2016-05-06 15:10:06 +0300

    Translated

---
+++ resources/lang/fi/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Salasanan on oltava vähintään kuusi merkkiä ja ottelu vahvistus.',
+    'reset' => 'Salasanasi on nollattu!',
+    'sent' => 'Meillä on e-postitetaan salasanasi palautuslinkki!',
+    'token' => 'Tämän salasanan palautustiedostoja on virheellinen.',
+    'user' => "Emme voi löytää käyttäjälle, että e-mail osoite.",
+
+];

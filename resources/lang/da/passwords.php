36e0f9a88e3211b42474328807ed8ae1d21c69cf
tarasomel <iamnato4ka@gmail.com>
2016-05-06 15:10:16 +0300

    Translated

---
+++ resources/lang/da/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Adgangskoden skal være på mindst seks tegn og matche bekræftelse.',
+    'reset' => 'Din adgangskode er blevet nulstillet!',
+    'sent' => 'Vi har sendt e-mail til nulstilling af adgangskoden link!',
+    'token' => 'Dette password reset token er ugyldige.',
+    'user' => "Vi kan ikke finde en bruger med denne e-mail-adresse.",
+
+];

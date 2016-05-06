bf492ad10e527188e2e2f3be1b8175e676cbcca2
tarasomel <iamnato4ka@gmail.com>
2016-05-06 15:10:31 +0300

    Translated

---
+++ resources/lang/ru/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Пароль должен содержать не менее шести символов и соответствовать тому подтверждение.',
+    'reset' => 'Ваш пароль сброшен!',
+    'sent' => 'У нас есть по электронной почте ссылка на восстановление пароля!',
+    'token' => 'Этот пароль сброс маркера является недействительным.',
+    'user' => "Мы не можем найти пользователя с этим адресом электронной почты.",
+
+];

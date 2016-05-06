dce2baa5abd467d53e4ad5d9ad787e9682036f2e
tarasomel <iamnato4ka@gmail.com>
2016-05-06 15:10:16 +0300

    Translated

---
+++ resources/lang/cs/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Hesla musí být alespoň šest znaků a zápas potvrzení.',
+    'reset' => 'Vaše heslo bylo resetováno!',
+    'sent' => 'Máme e-mailem váš odkaz pro obnovení hesla!',
+    'token' => 'Toto heslo obnovit token je neplatný.',
+    'user' => "Nemůžeme najít uživatele s e-mailovou adresu.",
+
+];

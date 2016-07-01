c7eb24e1286c324719b133c7342855f97d038fcf
Taras Omelianchuk <iamnato4ka@gmail.com>
2016-07-01 16:40:16 +0300

    Translated

--- uk/config/cache.php
+++ uk/config/cache.php
@@ -4,9 +4,9 @@
 <tr><th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th></tr>
 <tr><th align='center' bgcolor='#eeeeec'>#</th><th align='left' bgcolor='#eeeeec'>Time</th><th align='left' bgcolor='#eeeeec'>Memory</th><th align='left' bgcolor='#eeeeec'>Function</th><th align='left' bgcolor='#eeeeec'>Location</th></tr>
 <tr><td bgcolor='#eeeeec' align='center'>1</td><td bgcolor='#eeeeec' align='center'>0.0001</td><td bgcolor='#eeeeec' align='right'>640176</td><td bgcolor='#eeeeec'>{main}(  )</td><td title='/home/crowdin/otaras/www/index.php' bgcolor='#eeeeec'>../index.php<b>:</b>0</td></tr>
-<tr><td bgcolor='#eeeeec' align='center'>2</td><td bgcolor='#eeeeec' align='center'>0.0673</td><td bgcolor='#eeeeec' align='right'>19737320</td><td bgcolor='#eeeeec'>application->run(  )</td><td title='/home/crowdin/otaras/www/index.php' bgcolor='#eeeeec'>../index.php<b>:</b>8</td></tr>
-<tr><td bgcolor='#eeeeec' align='center'>3</td><td bgcolor='#eeeeec' align='center'>0.0802</td><td bgcolor='#eeeeec' align='right'>22948992</td><td bgcolor='#eeeeec'>controller->perform_action(  )</td><td title='/home/crowdin/otaras/src/application.php' bgcolor='#eeeeec'>../application.php<b>:</b>54</td></tr>
-<tr><td bgcolor='#eeeeec' align='center'>4</td><td bgcolor='#eeeeec' align='center'>0.0860</td><td bgcolor='#eeeeec' align='right'>23317840</td><td bgcolor='#eeeeec'>api_controller->do_export_file(  )</td><td title='/home/crowdin/otaras/src/core/controller.class.php' bgcolor='#eeeeec'>../controller.class.php<b>:</b>125</td></tr>
+<tr><td bgcolor='#eeeeec' align='center'>2</td><td bgcolor='#eeeeec' align='center'>0.0576</td><td bgcolor='#eeeeec' align='right'>19737320</td><td bgcolor='#eeeeec'>application->run(  )</td><td title='/home/crowdin/otaras/www/index.php' bgcolor='#eeeeec'>../index.php<b>:</b>8</td></tr>
+<tr><td bgcolor='#eeeeec' align='center'>3</td><td bgcolor='#eeeeec' align='center'>0.0635</td><td bgcolor='#eeeeec' align='right'>22949040</td><td bgcolor='#eeeeec'>controller->perform_action(  )</td><td title='/home/crowdin/otaras/src/application.php' bgcolor='#eeeeec'>../application.php<b>:</b>54</td></tr>
+<tr><td bgcolor='#eeeeec' align='center'>4</td><td bgcolor='#eeeeec' align='center'>0.0662</td><td bgcolor='#eeeeec' align='right'>23317928</td><td bgcolor='#eeeeec'>api_controller->do_export_file(  )</td><td title='/home/crowdin/otaras/src/core/controller.class.php' bgcolor='#eeeeec'>../controller.class.php<b>:</b>125</td></tr>
 </table></font>
 {
   "success":false,

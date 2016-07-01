1c1d1a439f50240557fbf09397e5f787933c5726
Taras Omelianchuk <iamnato4ka@gmail.com>
2016-07-01 16:01:28 +0300

    Translated

--- uk/config/cache.php
+++ uk/config/cache.php
@@ -1,81 +1,17 @@
-<?php
-
-return [
-
-    /*
-    |--------------------------------------------------------------------------
-    | Default Cache Store
-    |--------------------------------------------------------------------------
-    |
-    | This option controls the default cache connection that gets used while
-    | using this caching library. This connection is used when another is
-    | not explicitly specified when executing a given caching function.
-    |
-    */
-
-    'default' => env('CACHE_DRIVER', 'file'),
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cache Stores
-    |--------------------------------------------------------------------------
-    |
-    | Here you may define all of the cache "stores" for your application as
-    | well as their drivers. You may even define multiple stores for the
-    | same cache driver to group types of items stored in your caches.
-    |
-    */
-
-    'stores' => [
-
-        'apc' => [
-            'driver' => 'БТР',
-        ],
-
-        'array' => [
-            'driver' => 'масив',
-        ],
-
-        'database' => [
-            'driver' => 'базі',
-            'table' => 'кеш',
-            'connection' => null,
-        ],
-
-        'file' => [
-            'driver' => 'файл',
-            'path' => storage_path('рамки/кеш'),
-        ],
-
-        'memcached' => [
-            'driver' => 'як memcached',
-            'servers' => [
-                [
-                    'host' => env('MEMCACHED_HOST', '127.0.0.1'),
-                    'port' => env('MEMCACHED_PORT', 11211),
-                    'weight' => 100,
-                ],
-            ],
-        ],
-
-        'redis' => [
-            'driver' => 'рэдис',
-            'connection' => 'за замовчуванням',
-        ],
-
-    ],
-
-    /*
-    |--------------------------------------------------------------------------
-    | Cache Key Prefix
-    |--------------------------------------------------------------------------
-    |
-    | When utilizing a RAM based store such as APC or Memcached, there might
-    | be other applications utilizing the same cache. So, we'll specify a
-    | value to get prefixed to all our keys so we can avoid collisions.
-    |
-    */
-
-    'prefix' => 'що laravel',
-
-];
+<br />
+<font size='1'><table class='xdebug-error xe-fatal-error' dir='ltr' border='1' cellspacing='0' cellpadding='1'>
+<tr><th align='left' bgcolor='#f57900' colspan="5"><span style='background-color: #cc0000; color: #fce94f; font-size: x-large;'>( ! )</span> Fatal error: Call to undefined method api_controller::getId() in /home/crowdin/otaras/mvc/controllers/api.class.php on line <i>1609</i></th></tr>
+<tr><th align='left' bgcolor='#e9b96e' colspan='5'>Call Stack</th></tr>
+<tr><th align='center' bgcolor='#eeeeec'>#</th><th align='left' bgcolor='#eeeeec'>Time</th><th align='left' bgcolor='#eeeeec'>Memory</th><th align='left' bgcolor='#eeeeec'>Function</th><th align='left' bgcolor='#eeeeec'>Location</th></tr>
+<tr><td bgcolor='#eeeeec' align='center'>1</td><td bgcolor='#eeeeec' align='center'>0.0001</td><td bgcolor='#eeeeec' align='right'>640176</td><td bgcolor='#eeeeec'>{main}(  )</td><td title='/home/crowdin/otaras/www/index.php' bgcolor='#eeeeec'>../index.php<b>:</b>0</td></tr>
+<tr><td bgcolor='#eeeeec' align='center'>2</td><td bgcolor='#eeeeec' align='center'>0.0673</td><td bgcolor='#eeeeec' align='right'>19737320</td><td bgcolor='#eeeeec'>application->run(  )</td><td title='/home/crowdin/otaras/www/index.php' bgcolor='#eeeeec'>../index.php<b>:</b>8</td></tr>
+<tr><td bgcolor='#eeeeec' align='center'>3</td><td bgcolor='#eeeeec' align='center'>0.0802</td><td bgcolor='#eeeeec' align='right'>22948992</td><td bgcolor='#eeeeec'>controller->perform_action(  )</td><td title='/home/crowdin/otaras/src/application.php' bgcolor='#eeeeec'>../application.php<b>:</b>54</td></tr>
+<tr><td bgcolor='#eeeeec' align='center'>4</td><td bgcolor='#eeeeec' align='center'>0.0860</td><td bgcolor='#eeeeec' align='right'>23317840</td><td bgcolor='#eeeeec'>api_controller->do_export_file(  )</td><td title='/home/crowdin/otaras/src/core/controller.class.php' bgcolor='#eeeeec'>../controller.class.php<b>:</b>125</td></tr>
+</table></font>
+{
+  "success":false,
+  "error":{
+    "code":0,
+    "message":"Unknown internal error"
+  }
+}

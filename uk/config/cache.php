ea52605bedb2df651e60dd6a58a5856053e48e97
Taras Omelianchuk <iamnato4ka@gmail.com>
2016-07-01 13:49:48 +0300

    Translated

---
+++ uk/config/cache.php
@@ -0,0 +1,81 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Cache Store
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the default cache connection that gets used while
+    | using this caching library. This connection is used when another is
+    | not explicitly specified when executing a given caching function.
+    |
+    */
+
+    'default' => env('CACHE_DRIVER', 'file'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Stores
+    |--------------------------------------------------------------------------
+    |
+    | Here you may define all of the cache "stores" for your application as
+    | well as their drivers. You may even define multiple stores for the
+    | same cache driver to group types of items stored in your caches.
+    |
+    */
+
+    'stores' => [
+
+        'apc' => [
+            'driver' => 'БТР',
+        ],
+
+        'array' => [
+            'driver' => 'масив',
+        ],
+
+        'database' => [
+            'driver' => 'базі',
+            'table' => 'кеш',
+            'connection' => null,
+        ],
+
+        'file' => [
+            'driver' => 'файл',
+            'path' => storage_path('рамки/кеш'),
+        ],
+
+        'memcached' => [
+            'driver' => 'як memcached',
+            'servers' => [
+                [
+                    'host' => env('MEMCACHED_HOST', '127.0.0.1'),
+                    'port' => env('MEMCACHED_PORT', 11211),
+                    'weight' => 100,
+                ],
+            ],
+        ],
+
+        'redis' => [
+            'driver' => 'рэдис',
+            'connection' => 'за замовчуванням',
+        ],
+
+    ],
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cache Key Prefix
+    |--------------------------------------------------------------------------
+    |
+    | When utilizing a RAM based store such as APC or Memcached, there might
+    | be other applications utilizing the same cache. So, we'll specify a
+    | value to get prefixed to all our keys so we can avoid collisions.
+    |
+    */
+
+    'prefix' => 'що laravel',
+
+];

d1dce46f2eff919e3f9d42c162b8e87a74cea9bb
Taras Omelianchuk <iamnato4ka@gmail.com>
2016-07-20 14:10:52 +0300

    Translated

---
+++ resources/lang/vi-rVN/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Mật khẩu phải có ít nhất là sáu nhân vật này và hợp xác nhận.',
+    'reset' => 'Mật mã của anh đã được đặt lại!',
+    'sent' => 'Chúng ta có đãgởi mật mã của anh cần khởi động lại, link!',
+    'token' => 'Mật khẩu này lại lời là không hợp lệ.',
+    'user' => "Chúng ta không thể tìm thấy một người dùng với địa chỉ e-mail.",
+
+];

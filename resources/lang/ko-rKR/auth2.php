5245385494351826f348f7b342ab500b0839ce5c
Taras Omelianchuk <iamnato4ka@gmail.com>
2016-07-20 14:10:39 +0300

    Translated

---
+++ resources/lang/ko-rKR/auth2.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => '이러한 자격이 일치하지 않습니다.',
+    'throttle' => '너무 많은 로그인을 시도합니다. 에 다시 시도해 주세요:초 초니다.',
+
+];

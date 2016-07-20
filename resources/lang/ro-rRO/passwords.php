c1802966d8690d4d4ba401b71d6831aa1e1a49fc
Taras Omelianchuk <iamnato4ka@gmail.com>
2016-07-20 14:10:30 +0300

    Translated

---
+++ resources/lang/ro-rRO/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Parolele trebuie să fie de cel puțin șase caractere și se potrivesc de confirmare.',
+    'reset' => 'Parola a fost resetată!',
+    'sent' => 'Am trimis un e-mail de resetare a parolei dumneavoastră link-ul!',
+    'token' => 'Această simbolul de reinițializare a parolei este invalid.',
+    'user' => "Nu putem găsi un utilizator cu adresa de e-mail.",
+
+];

f9fae7d02ae21a078a859a403542def9f0a73f1d
Taras Omelianchuk <iamnato4ka@gmail.com>
2016-07-20 14:11:47 +0300

    Translated

---
+++ resources/lang/de-rDE/auth2.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Diese Anmeldeinformationen entsprechen nicht unseren Aufzeichnungen.',
+    'throttle' => 'Zu viele login-versuche. Bitte versuchen Sie es erneut in :Sekunden Sekunden.',
+
+];

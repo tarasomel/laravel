6eb0f67f38bc5c5501c32549ea49167c22c57613
Taras Omelianchuk <iamnato4ka@gmail.com>
2016-07-20 14:10:33 +0300

    Translated

---
+++ resources/lang/ja-rJP/auth2.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'これらの資格が一致しない当社の記録です。',
+    'throttle' => '多くのログインを試みます。 て再度試してみてください：秒です。',
+
+];

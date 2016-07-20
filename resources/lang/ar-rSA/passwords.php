c222c33ec9aa9e28bbd292f6a236eac7c015a853
Taras Omelianchuk <iamnato4ka@gmail.com>
2016-07-20 14:11:37 +0300

    Translated

---
+++ resources/lang/ar-rSA/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'يجب أن تكون كلمات السر على الأقل ستة أحرف المباراة تأكيد.',
+    'reset' => 'كلمة المرور الخاصة بك قد تم إعادة تعيين!',
+    'sent' => 'لدينا البريد الإلكتروني الخاص بك رابط إعادة تعيين كلمة المرور!',
+    'token' => 'هذا إعادة تعيين كلمة المرور رمز غير صالح.',
+    'user' => "ونحن لا يمكن العثور على المستخدم مع هذا عنوان البريد الإلكتروني.",
+
+];

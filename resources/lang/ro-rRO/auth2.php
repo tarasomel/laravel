2f145dd559775b05fef52e68b2cf1266241b4745
Taras Omelianchuk <iamnato4ka@gmail.com>
2016-07-20 14:10:29 +0300

    Translated

---
+++ resources/lang/ro-rRO/auth2.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Aceste acreditări nu se potrivesc înregistrările noastre.',
+    'throttle' => 'Prea multe încercări de autentificare. Vă rugăm să încercați din nou în :secunde secunde.',
+
+];

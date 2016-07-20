2121a69608dd0f4f792992207e1b0f80dc3df986
Taras Omelianchuk <iamnato4ka@gmail.com>
2016-07-20 14:11:13 +0300

    Translated

---
+++ resources/lang/it-rIT/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Le password devono essere di almeno sei caratteri e soddisfare la conferma.',
+    'reset' => 'La tua password è stata reimpostata!',
+    'sent' => 'Abbiamo inviato e-mail il link di reimpostazione password!',
+    'token' => 'Questa password reset token non valido.',
+    'user' => "Non siamo in grado di trovare un utente con l'indirizzo di posta elettronica.",
+
+];

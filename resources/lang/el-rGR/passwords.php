05a2c08fc8c562b900f1b8f8ee19515de128954d
Taras Omelianchuk <iamnato4ka@gmail.com>
2016-07-20 14:11:44 +0300

    Translated

---
+++ resources/lang/el-rGR/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Οι κωδικοί πρόσβασης πρέπει να είναι τουλάχιστον έξι χαρακτήρες και να ταιριάζει με την επιβεβαίωση.',
+    'reset' => 'Αλλάξετε τον κωδικό πρόσβασής σας!',
+    'sent' => 'Έχουμε το e-mail σας σύνδεσμο επαναφοράς κωδικού!',
+    'token' => 'Αυτό διακριτικού επαναφοράς κωδικού πρόσβασης δεν είναι έγκυρος.',
+    'user' => "Δεν μπορούμε να βρούμε ένα χρήστη με e-mail.",
+
+];

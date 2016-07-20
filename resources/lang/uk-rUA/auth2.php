66696f5e12d15c682e63311b937fac24ac0ccf35
Taras Omelianchuk <iamnato4ka@gmail.com>
2016-07-20 14:10:57 +0300

    Translated

--- resources/lang/uk-rUA/auth2.php
+++ resources/lang/uk-rUA/auth2.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Ці облікові дані не співпадають з нашими записами.',
+    'throttle' => 'Занадто багато спроб входу. Будь ласка, спробуйте через :секунд секунд.',
 
 ];

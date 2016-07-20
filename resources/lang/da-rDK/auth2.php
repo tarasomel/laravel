947d84ea47e8a96eb65398ee188d53ad9a0f657a
Taras Omelianchuk <iamnato4ka@gmail.com>
2016-07-20 14:11:22 +0300

    Translated

---
+++ resources/lang/da-rDK/auth2.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+    'failed' => 'Disse legitimationsoplysninger ikke matche vores registreringer.',
+    'throttle' => 'For mange log ind forsøg. Prøv venligst igen :sekunder sekunder.',
+
+];

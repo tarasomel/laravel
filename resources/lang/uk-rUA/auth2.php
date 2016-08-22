3f1f64b694d1c1bc1514ae8fc23203b5c6c84162
Taras Omelianchuk <iamnato4ka@gmail.com>
2016-08-22 16:53:39 +0300

    New translations

--- resources/lang/uk-rUA/auth2.php
+++ resources/lang/uk-rUA/auth2.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'Ці облікові дані не співпадають з нашими записами.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
     'crocodile' => 'crocodile rabbit',
 
